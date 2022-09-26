25a06d29a225c4ecd352684ab22ddf28866ce635
Tristan Döhl <tristan.doehl@holy-mail.de>
2022-09-26 16:11:09 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const Nav = ({children}) => {
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <BrowserRouter >
+      <BrowserRouter basename={'/isf-simulator/'} >
         <Routes>
           <Route path={'/'} element={<Nav><HomePage /></Nav>} />
           <Route path={'/mqtt'} element={<Nav><MqttTestPage/></Nav>} />
